29ed958a6490236e7c77070f53ddd825872f8364
nicjrk <116735056+nicjrk@users.noreply.github.com>
2025-04-08 11:20:36 +0300

    schimbare apii

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  const API_URL = "https://script.google.com/macros/s/AKfycbxc0Tv1KfM7YpfjeH-39VNzOTy7kkwDjwOdjf9GWYv-I8W-W0agbxnFclH1ZOkOdi-hqg/exec";
+  const API_URL = "https://script.google.com/macros/library/d/1xXiVx_Fb0PNo8Pap8XoRvtcwyknLVFfL03_tOXW3B-7a-W4omIXa6He6/2";
 
   useEffect(() => {
     fetchOccupiedSeats();
